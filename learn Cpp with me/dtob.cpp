d2f1fef4376e118a95eaa5c3d7d06964d2783b49
Raghav Rawal <raghavrawal1831@gmail.com>
2025-08-25 10:01:22 +0530

    Add files via upload

---
+++ learn Cpp with me/dtob.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int ans = 0;
+    int i =0;
+
+    while (n != 0)
+    {
+        /* code */
+        int bit = n&1;//and opr bw n - 1);
+        ans = (bit*pow(10,i)+ans);
+        
+        n   = n>>1;
+        i++;
+    }
+        cout<<ans<<endl;
+    
+}
