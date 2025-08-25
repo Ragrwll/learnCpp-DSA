675d41f23529441cd248f392aefe3c2d62636251
Raghav Rawal <raghavrawal1831@gmail.com>
2025-08-25 10:22:08 +0530

    Update dtob.cpp

--- learn Cpp with me/dtob.cpp
+++ learn Cpp with me/dtob.cpp
@@ -5,19 +5,24 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
+    int num = abs(n);
 
     int ans = 0;
     int i =0;
 
-    while (n != 0)
+    while (num != 0)
     {
         /* code */
-        int bit = n&1;//and opr bw n - 1);
+        int bit = num&1;//and opr bw n - 1);
         ans = (bit*pow(10,i)+ans);
         
-        n   = n>>1;
+        num   = num>>1;
         i++;
     }
+    if(n<0){
+        cout<<"-"<<ans<<endl;
+    }else{
         cout<<ans<<endl;
-    
+    }
+
 }
